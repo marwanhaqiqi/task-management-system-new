52d2f2bc0deb8d2eb50ac33e9ea4d45a41ded711
Gallung Marwan <elmarwansyah2003@gmail.com>
2025-06-14 13:50:28 +0700

    Add homepage route and catch-all route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -7,11 +7,13 @@ import AddTasklist from "./pages/AddTask";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/add" element={<AddTasklist />} />
       <Route path="/edit/:taskId" element={<AddTasklist />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
